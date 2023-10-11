0fa58b3259d3fe9f77cb749037e6a9681a9cb1b8
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2023-10-11 10:37:35 +0300

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,31 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ERROR 404</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/css/404.css">
+  </head>
+  <body>
+  <main class="bl_page404">
+  <h1>Error 404. The page does not exist</h1>
+  <p>Sorry! The page you are looking for can not be found. Perhaps the page you requested was moved or deleted. It is also possible that you made a small typo when entering the address. Go to the main page.
+  </p>
+  <div class="bl_page404__wrapper">
+    <img src="https://github.com/BlackStar1991/Pictures-for-sharing-/blob/master/404/bigBoom/cloud_warmcasino.png?raw=true" alt="cloud_warmcasino.png">
+    <div class="bl_page404__el1"></div>
+    <div class="bl_page404__el2"></div>
+    <div class="bl_page404__el3"></div>
+    <a class="bl_page404__link" href="index.php">go home</a>
+  </div>
+</main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
+
+
+
+
